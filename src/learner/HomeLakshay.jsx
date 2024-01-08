52f3d82a8c7bf3bd482b7020e3eb9b37855151b9
kartikey-mittal <g6.kartikey@gmail.com>
2024-01-09 02:35:53 +0530

    REQUEST

--- src/learner/HomeLakshay.jsx
+++ src/learner/HomeLakshay.jsx
@@ -19,7 +19,7 @@ const HomeLakshay = () => {
         flexDirection: 'column',
         alignItems: 'flex-start', // Align items to the start (top)
         overflow: 'hidden', // Hide overflow content
-        backgroundColor:'#F3F6FC'
+        backgroundColor: '#F3F6FC'
     };
 
     const headingStyle = {
@@ -48,31 +48,86 @@ const HomeLakshay = () => {
             <div style={contentStyle}>
                 <div style={headingStyle}>⚡⚡Hi Kartikey Mittal, what is your question today?</div>
                 <div style={mainboxStyle}>
-                <section style={{ margin: '5rem 0.75rem' }}>
-       <div style={{ display: 'flex' ,border: 'solid .1px', borderRadius: '9px',borderBlockColor:'#9a7d7d'}}>
-           <div style={{ padding: '1rem', width: '40%', height: '25%',  }}>
-             <img style={{ height: '18rem', width: '100%', objectFit: 'cover',borderRadius: '9px' }} src="https://imgs.search.brave.com/_3nOUpPG1H3D6I1X7G04vjqfBw-EmkY41kZ9EPkDIEk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9nbG9i/YWwuZGlzY291cnNl/LWNkbi5jb20vZnJl/ZWNvZGVjYW1wL29w/dGltaXplZC80WC8y/LzgvMy8yODMyZjdm/MWNkOTlkNTE2M2Yy/NmU1MGY4OTAwZjVk/Mjg0Mzk2MjcwXzJf/NjYyeDUwMC5wbmc" alt="An image" />
-           </div>
+                    <section style={{ margin: '20px', backgroundColor: '#F9F9F9', display: 'flex',}}>
+                        <div style={{ display: 'flex', border: 'solid .1px', borderRadius: '20px', borderBlockColor: '#9a7d7d', backgroundColor: 'transparent', width: '100%' }}>
+                            <div style={{ padding: '1rem', width: '30%', height: '25%', }}>
+                                <img style={{ height: '25vh', width: '100%', objectFit: 'cover', borderRadius: '9px' }} src="https://imgs.search.brave.com/_3nOUpPG1H3D6I1X7G04vjqfBw-EmkY41kZ9EPkDIEk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9nbG9i/YWwuZGlzY291cnNl/LWNkbi5jb20vZnJl/ZWNvZGVjYW1wL29w/dGltaXplZC80WC8y/LzgvMy8yODMyZjdm/MWNkOTlkNTE2M2Yy/NmU1MGY4OTAwZjVk/Mjg0Mzk2MjcwXzJf/NjYyeDUwMC5wbmc" alt="An image" />
+                            </div>
 
-           <div style={{ padding: '3rem',  color: 'black', display: 'grid', gridTemplateRows: '1fr auto', height: '14rem' }}>
-             <h3>Getting error in useeffect...not executing properly. Please help.</h3>
-             <div style={{  display: 'flex', flexDirection: 'column',  alignItems: 'center',alignItems: 'start'}}>
-               <p style={{ color: '#9A7D7D' }}>lakshay jain</p>
-               <button style={{cursor:'pointer', width: '6rem', height: '2.25rem', border: 'solid 1px', borderRadius: '9999px', display: 'flex', alignItems: 'center', justifyContent: 'center' ,borderBlockColor:'#7D716A'}}>react</button>
-             </div>
+                            <div style={{ padding: '10px', color: 'black', display: 'grid', gridTemplateRows: '1fr auto', backgroundColor: 'transparent', width: '100%' }}>
 
-             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'end' }}>
-               <button style={{color: 'white', cursor:'pointer',backgroundColor: '#EA775C', width: '6rem', height: '2.25rem', border: 'solid .1px', borderRadius: '9999px', display: 'flex', alignItems: 'center', justifyContent: 'center',borderBlockColor:'#EA775C'}}>ignore</button>
-               <button style={{color: 'white',cursor:'pointer', backgroundColor: '#4285F4', width: '6rem', height: '2.25rem', border: 'solid .1px', borderRadius: '9999px', display: 'flex', alignItems: 'center', justifyContent: 'center',marginLeft:'8px',borderBlockColor:'#4285F4' }}>connect</button>
-             </div>
-           </div>
-       </div>
-   </section>
+                                <p style={{ backgroundColor: 'transparent', textAlign: 'start', fontFamily: 'DMM', alignItems: 'center', fontSize: '100%' ,letterSpacing: '0.8px',wordSpacing: '1px',}}>
+                                    Getting error in useEffect...not executing properly. Please help.Getting error in useEffect...not executing properly. Please help.Getting error in useEffect...not executing properly. Please help.
+                                    ferfnrejkn
+                                </p>
+
+
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', alignItems: 'start' }}>
+                                    <p style={{ color: '#9A7D7D', fontFamily: 'DMM' }}>Lakshay Jain</p>
+                                    <button
+                                        style={{
+                                            backgroundColor: 'white',
+                                            color: '#4285F4',
+                                            borderRadius: 100,
+                                            border: '1px solid #7D716A', // Set border to solid black
+                                            fontFamily: 'DMM',
+                                            padding: '8px 15px',
+                                            outline: 'none',
+                                            width: 'auto',
+                                            
+                                        }}
+                                    >
+                                        React
+                                    </button>
+
+                                </div>
+
+                                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'end' ,paddingRight:30}}>
+                                    <button
+                                        style={{
+                                            backgroundColor: '#EA775C',
+                                            color: '#fff',
+                                            borderRadius: 100,
+                                            border: '#7D716A',
+                                            borderWidth: '0.2px',
+                                            fontFamily: 'DMM',
+                                            padding: '10px',
+                                            outline: 'none',
+                                            width: 'auto',
+                                            paddingLeft: 20,
+                                            paddingRight: 20,
+                                            fontSize: 15,
+                                            cursor:'pointer'
+                                        }}  >
+                                        Ignore  </button>
+
+                                    <button
+                                        style={{
+                                            backgroundColor: '#4285F4',
+                                            color: '#fff',
+                                            borderRadius: 100,
+                                            border: '#7D716A',
+                                            borderWidth: '0.2px',
+                                            fontFamily: 'DMM',
+                                            padding: '10px',
+                                            outline: 'none',
+                                            width: 'auto',
+                                            paddingLeft: 20,
+                                            paddingRight: 20,
+                                            marginLeft: 10,
+                                            fontSize: 15,
+                                            cursor:'pointer'
+                                        }}  >
+                                        Connect  </button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-              
-                
+
+
             </div>
-           
+
         </div>
     );
 };
