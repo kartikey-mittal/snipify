0cde24559ca9f48ea8db121c97cd766d952f5f97
kartikey-mittal <g6.kartikey@gmail.com>
2024-01-19 20:36:03 +0530

    it should work

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
+import { getMessaging } from "firebase/messaging";
 import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,4 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
-export { db, storage,collection };
+const messaging = getMessaging(app);
+
+
+export { db, storage,collection,messaging };
